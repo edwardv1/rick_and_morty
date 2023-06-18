53fd02dabc53e9c344b73c47c374809059bfa1b7
Edward Vera <edwardismael1@gmail.com>
2023-06-18 20:21:30 -0300

    conexion con el server

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,17 @@
 const server = require("./app.js");
 const PORT = 3001;
+//importo instancia de sequelize con conn
+const { conn } = require("./DB_connection");
 
-server.listen(PORT, () => {
-   console.log('Server raised in port: ' + PORT);
-});
+//cambiar el valor de force a false cuando tenga el server listo
+conn.sync({ force: true }) //devuelve una promesa
+   .then(()=> {
+      server.listen(PORT, async () => {
+         console.log('Server raised in port: ' + PORT);
+      })
+   })
+   .catch(error => console.log(error.message))
+
+
+
+//await conn.sync({ force: true});
